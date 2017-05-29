a242fd9629efc7fd7d2e14da8c9787b8c726a4ab
LilSaint <pamela.carric@gmail.com>
2017-05-29 18:53:16 +0200

    Installation de socket

--- public/server.js
+++ public/server.js
@@ -2,21 +2,44 @@
 
 var express,
     app,
+    server,
     http,
-    server;
+    socket;
 //
-// On initialise l'application avec le framework Express
-// Et la bibliothéque http intégrée à node
-express = require('express');
+// // On initialise l'application avec le framework Express
+// // Et la bibliothéque http intégrée à node
+// express = require('express');
+// app = express();
+// // Création du serveur
+// server = require('http').createServer(app);
 //
-app = express();
+// // Lancement du serveur
+// app.get('/', function (req, res) {
+//     'use strict';
+//     res.sendfile(__dirname + '/index.html');
+//     app.use(express.static(__dirname, 'css'));
+//     app.use(express.static(__dirname, 'js'));
+// });
 //
-server = require('http').createServer(app);
+// // Rattachement du serveur à un port
+// server.listen(8080);
+//
+// // Init Socket.io
+// socket = require('socket.io')(server);
 
-app.get('/', function (req, res) {
+http = require('http');
+socket = require('socket.io');
+express = require('express');
+app = express();
+
+server = http.createServer(function (req, res) {
     'use strict';
-    res.sendfile(__dirname + '/index.html');
-    app.use(express.static(__dirname, 'css'));
-    app.use(express.static(__dirname, 'js'));
+    console.log('Le serveur est lancé');
+    // res.sendfile(__dirname + '/index.html');
+    // app.use(express.static(__dirname, 'css'));
+    // app.use(express.static(__dirname, 'js'));
 });
+
 server.listen(8080);
+
+socket.listen(server);
