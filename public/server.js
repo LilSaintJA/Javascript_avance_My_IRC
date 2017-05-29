afa16299d1b4f8e3fbdc4bf2c12003430da1778b
LilSaint <pamela.carric@gmail.com>
2017-05-29 19:13:50 +0200

    Connection à Socket OK

--- public/server.js
+++ public/server.js
@@ -3,43 +3,28 @@
 var express,
     app,
     server,
-    http,
-    socket;
+    io;
 //
-// // On initialise l'application avec le framework Express
-// // Et la bibliothéque http intégrée à node
-// express = require('express');
-// app = express();
-// // Création du serveur
-// server = require('http').createServer(app);
-//
-// // Lancement du serveur
-// app.get('/', function (req, res) {
-//     'use strict';
-//     res.sendfile(__dirname + '/index.html');
-//     app.use(express.static(__dirname, 'css'));
-//     app.use(express.static(__dirname, 'js'));
-// });
-//
-// // Rattachement du serveur à un port
-// server.listen(8080);
-//
-// // Init Socket.io
-// socket = require('socket.io')(server);
-
-http = require('http');
-socket = require('socket.io');
+// On initialise l'application avec le framework Express
+// Et la bibliothéque http intégrée à node
 express = require('express');
 app = express();
+// Création du serveur
+server = require('http').createServer(app);
 
-server = http.createServer(function (req, res) {
+// Lancement du serveur
+app.get('/', function (req, res) {
     'use strict';
-    console.log('Le serveur est lancé');
-    // res.sendfile(__dirname + '/index.html');
-    // app.use(express.static(__dirname, 'css'));
-    // app.use(express.static(__dirname, 'js'));
+    res.sendfile(__dirname + '/index.html');
+    app.use(express.static(__dirname, 'css'));
+    app.use(express.static(__dirname, 'js'));
 });
 
+// Rattachement du serveur à un port
 server.listen(8080);
 
-socket.listen(server);
+// Init Socket.io
+io = require('socket.io')(server);
+io.on('connection', function (socket) {
+   console.log('Client connecté');
+});
