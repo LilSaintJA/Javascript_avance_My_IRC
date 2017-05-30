5fdaba6f86fa8a9e58c37f4b5682ec68834d9721
LilSaint <pamela.carric@gmail.com>
2017-05-30 18:34:11 +0200

    Ajout de js hors node

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+/*global console, $, jQuery */
+
+(function ($) {
+    'use strict';
+
+    var btn = $('.loginBtn');
+    console.log(btn);
+
+    btn.click(function () {
+       // var form = $('.hidden');
+       //
+       // form.toggle(display);
+       // console.log(form);
+        $('.hidden').toggle('slow');
+    });
+})(jQuery);
