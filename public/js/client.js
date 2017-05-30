e832b3ba3f7a2f236706c8d4f1dd67fa23a56659
LilSaint <pamela.carric@gmail.com>
2017-05-30 10:43:35 +0200

    Connection du site à Socket

--- public/js/client.js
+++ public/js/client.js
@@ -1,2 +1,6 @@
-console.log("ready");
-var socket = io();
+/*global console, $, jQuery */
+(function ($) {
+    'use strict';
+    console.log("jQuery Is Ready");
+    var socket = io('http://localhost:8080');
+})(jQuery);
