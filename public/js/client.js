90740d57a1c64f502ce97a71bc6912213b488eeb
LilSaint <pamela.carric@gmail.com>
2017-05-30 19:28:45 +0200

    Evenement login done

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,24 @@
 /*global console, $, jQuery */
 (function ($) {
     'use strict';
-    console.log("jQuery Is Ready");
+
+    function log(D) {
+        console.log(D);
+    }
+
+    log("jQuery Is Ready");
+
     var socket = io('http://localhost:8080');
+
+    $('#loginForm').submit(function (evt) {
+       evt.preventDefault();
+
+       // Envoie d'événement login avec socket au server
+       socket.emit('login', {
+           pseudo   : $('#pseudo').val(),
+           mail     : $('#mdp').val()
+       });
+    });
+
+    log(socket);
 })(jQuery);
