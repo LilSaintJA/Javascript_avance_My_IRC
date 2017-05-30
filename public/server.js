1eb067cc32c66898c5d9e4601880ca3243b74ff2
LilSaint <pamela.carric@gmail.com>
2017-05-30 23:20:03 +0200

    Connection à la BDD w/mongo, récup des infos de la BDD OK

--- public/server.js
+++ public/server.js
@@ -3,7 +3,8 @@
 var express,
     app,
     http,
-    io;
+    io,
+    mongoClient;
 //
 // On initialise l'application avec le framework Express
 // Et la bibliothéque http intégrée à node
@@ -16,6 +17,9 @@ http = require('http').Server(app);
 // Init socket
 io = require('socket.io')(http);
 
+mongoClient = require('mongodb').MongoClient;
+// console.log(mongoClient);
+
 // Lancement du serveur
 app.get('/', function (req, res) {
     'use strict';
@@ -26,6 +30,30 @@ app.get('/', function (req, res) {
 
 // On écoute le serveur
 http.listen(8080);
+
+// Connection MongoDB
+mongoClient.connect("mongodb://localhost:27017/tchatJS", function (error, db) {
+
+    var collection = db.collection('users');
+
+    if (error) return console.error('Connection failed', error);
+
+    // console.log("Connecté à la base de données 'tchatJS'");
+
+    collection.find().toArray(function (error, results) {
+        if (error) throw error;
+        // console.log(results);
+
+        results.forEach(function (obj) {
+            // console.log('je passe dans la boucle');
+            console.log('ID : ' + obj._id.toString());
+            console.log('Pseudo : ' + obj.pseudo);
+            console.log('Mdp : ' + obj.mdp);
+        });
+    });
+
+    db.close();
+});
 
 // Chaque user va avoir sa propre connection socket
 io.on('connection', function (socket) {
