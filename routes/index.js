b002f89b2a575186f377e64b2099b24d2fd27cbd
LilSaint <pamela.carric@gmail.com>
2017-06-03 18:00:29 +0200

    Signup w/mongoose OK

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb');
-var assert = require('assert');
+var mongoose = require('mongoose');
+// var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/tchatJS';
+// var url = 'mongodb://localhost:27017/tchatJS';
+
+mongoose.connect('mongodb://localhost:27017/tchatJS');
+
+var userSchema = new mongoose.Schema({
+    pseudonyme: { type: String, unique: true },
+    email: String,
+    pass: String,
+    confirmPassword: String,
+    dob: Date
+}, {collection: 'signup'});
+
+var UserData = mongoose.model('userSchema', userSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.post('/', function (req, res, next) {
@@ -18,15 +30,20 @@ router.post('/', function (req, res, next) {
        confirmPassword: req.body.confirmPassword,
        dob: req.body.dob
    };
+
+    var user = new UserData(item);
+    user.save();
    
-   mongo.connect(url, function (error, db) {
+   /*mongo.connect(url, function (error, db) {
       assert.equal(null, error);
       db.collection('signup').insertOne(item, function (err, results) {
          assert.equal(null, err);
          console.log('Item inserted');
+         res.render('index', { title: 'Inscription réussit' });
          db.close();
       });
-   });
+   });*/
+   res.redirect('/');
 });
 
 module.exports = router;
