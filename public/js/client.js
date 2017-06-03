575bac366a84112a2e7d3bc08b09db1fbf2e87f4
LilSaint <pamela.carric@gmail.com>
2017-06-03 18:01:36 +0200

    Fichier gestion event client

--- public/js/client.js
+++ public/js/client.js
@@ -8,34 +8,36 @@
 
     log("jQuery Is Ready");
 
-    var socket = io();
-
-    $('#loginForm').submit(function (evt) {
-       evt.preventDefault();
-
-       // Envoie d'événement login avec socket au server
-       socket.emit('login', {
-           pseudo    : $('#pseudo').val(),
-           mdp       : $('#mdp').val()
-       });
-    });
-
-    socket.on('logged', function () {
-       $('#loginForm').toggle('slow');
-    });
-
-
-    /**
-     * Gestion des connectés
-     */
-    socket.on('newusr', function (user) {
-        // $('.users').append('<img src="' + user.avatar +'">');
-        $('.users').append('<div id="' + user.id + '"><p>' + user.pseudo + '</p></div>');
-        log('Nouvel utilisateur');
-    });
-
-    socket.on('disUser', function (user) {
-        $('#' + user.id).remove();
-    });
+    // var socket = io();
+    // console.log(socket);
+    //
+    // $('#loginForm').submit(function (evt) {
+    //    evt.preventDefault();
+    //
+    //    // Envoie d'événement login avec socket au server
+    //    socket.emit('login', {
+    //        pseudo    : $('#pseudo').val(),
+    //        mdp       : $('#mdp').val()
+    //    });
+    // });
+    //
+    // socket.on('logged', function () {
+    //    $('#loginForm').toggle('slow');
+    // });
+    //
+    //
+    // /**
+    //  * Gestion des connectés
+    //  */
+    // socket.on('newusr', function (user) {
+    //     console.log(user);
+    //     // $('.users').append('<img src="' + user.avatar +'">');
+    //     $('.users').append('<div><p>' + user.pseudo + '</p></div>');
+    //     log('Nouvel utilisateur');
+    // });
+    //
+    // socket.on('disUser', function (user) {
+    //     $('#' + user.id).remove();
+    // });
 
 })(jQuery);
