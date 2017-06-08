5ac32e69f3960b8d63ed346c9a634221a3cdd6a4
LilSaint <pamela.carric@gmail.com>
2017-06-08 10:45:16 +0200

    Modification control des formulaires

--- public/js/app.js
+++ public/js/app.js
@@ -13,11 +13,12 @@
         console.log('pout');
         $('.hiddenLogin').animate({height: "toggle", opacity: "toggle"}, "slow");
         $('.hiddenSignup').hide();
+        $('.socialBtn').show();
     });
 
     signupLink.click(function () {
        $('.hiddenSignup').animate({height: "toggle", opacity: "toggle"}, "slow");
-       $('.hiddenLogin').hide();
+       $('.hiddenLogin, .socialBtn').hide();
     });
 
     // $('.datepicker').pickadate({
