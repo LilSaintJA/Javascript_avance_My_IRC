e4632bba1216b4edc2f97066b2417ce8eb057e21
LilSaint <pamela.carric@gmail.com>
2017-06-11 11:53:53 +0200

    Ajout de création des channels via Socket

--- app/socket/index.js
+++ app/socket/index.js
@@ -4,7 +4,7 @@ var config  = require('../config');
 var redis   = require('redis').createClient();
 var adapter = require('socket.io-redis');
 
-// var Channel = require('../models/channel');
+var Channel = require('../models/channel');
 
 /**
  * All code for emitting and listening to Socket events
@@ -13,6 +13,30 @@ var adapter = require('socket.io-redis');
  */
 var ioEvents = function (io) {
 
+    /**
+     * Create a custum "channels" namespace
+     * Cela permet d'isoler les connections pour les channels
+     */
+    io.of('/channels').on('connection', function (socket) {
+
+        socket.on('createChannel', function (title) {
+            Channel.findOne({'title': new RegExp('^' + title + '$', 'i')}, function (err, channel) {
+                if (err) throw err;
+
+                if (channel) {
+                    socket.emit('updateChannelList', { error: "La channel existe déjà" });
+                } else {
+                    Channel.create({
+                        title: title
+                    }, function (err, newChannel) {
+                        if (err) throw err;
+                        socket.emit('updateChannelList', newChannel);
+                        socket.broadcast.emit('updateChannelList', newChannel);
+                    });
+                }
+            });
+        });
+    });
 };
 
 /**
@@ -30,10 +54,10 @@ var init = function (app) {
 
     io.use((socket, next) => {
         require('../session')(socket.request, {}, next);
-    });
+});
 
     // Define all Events
-    // ioEvents(io);
+    ioEvents(io);
 
     // The server object will be then used to list to a part number
     return server;
